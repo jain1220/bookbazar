a6c02f0fb411245eba8320d62e6d800f076de902
jain1220 <103555651+jain1220@users.noreply.github.com>
2022-04-29 15:05:40 +0530

    Update HomeController.java

--- src/main/java/com/codelpanet/Controller/HomeController.java
+++ src/main/java/com/codelpanet/Controller/HomeController.java
@@ -1,9 +1,17 @@
 package com.codelpanet.Controller;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import javax.servlet.http.HttpServletRequest;
+
 
 
 
@@ -12,10 +20,66 @@ import javax.servlet.http.HttpServletRequest;
 public class HomeController {
 	
 	@PostMapping("/signup")
-	public String signup(HttpServletRequest req)
-	{
-		
+	public String signup(HttpServletRequest req)throws SQLException, ClassNotFoundException
+	
+		{
+			String email=req.getParameter("email");
+			String psw=req.getParameter("psw");
+			String name=req.getParameter("name");
+			/*String date=req.getParameter("date");*/
+		    Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/application","root","root");
+			Statement stmt=con.createStatement();
+			String query = "select * from bookbazar where email='"+email+"'";
+			ResultSet rs =  stmt.executeQuery(query);
+			if(rs.next()==true)
+			{
+				req.setAttribute("test1", "you are already signed up");
+			}
+			else
+			{
+				String query1="insert into bookbazar values('"+name+"','"+email+"','"+psw+"')";
+				int row= stmt.executeUpdate(query1);
+				if(row>=1)
+				{
+					req.setAttribute("test1","you signed up successfully");
+				}
+			}
+			return "paypal";
+		}
+
+	@PostMapping("/login") 
+	  public String login(HttpServletRequest req) throws SQLException, ClassNotFoundException { 
+	  String email=req.getParameter("email"); 
+	  String psw=req.getParameter("psw");
+	  String name=req.getParameter("name"); 
+	  /*String date=req.getParameter("date");*/
+	  Class.forName("com.mysql.jdbc.Driver");
+	  Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/application","root","root");
+	  Statement stmt=con.createStatement();
+	  String query = "select * from bookbazar where email='"+email+"'";
+	  ResultSet rs =  stmt.executeQuery(query); 
+	  if(rs.next())
+	    {
+		  if((rs.getString("Password")).equals(psw)) {
+				req.setAttribute("test1","you are successfully logged in");
+				}
+		  else 
+				{
+					req.setAttribute("test1","your psw is not correct please check");
+				}
+	    }
+	  else
+	  {
+		  req.setAttribute("test1","you are not signed up");
+		  
+	  }
+	  return "paypal";
+	  
 	}
+
 }
+
+
 	
 	
